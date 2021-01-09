b2e015f7461d293d84ca788208e98446b509279c
Eddie <eddie.jostell@gmail.com>
2021-01-09 04:27:28 +0100

    Clean slate

--- src/components/Container/Container.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Container.scss';
-
-function Container(props: { children: React.ReactNode }) {
-  return <div className="container-fluid">{props.children}</div>;
-}
-
-export default Container;
