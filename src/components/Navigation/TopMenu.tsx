76481fc9fad2ff2b65b96a3c165f2c7b6ca42fa3
Eddie <eddie.jostell@gmail.com>
2021-12-10 00:07:54 +0100

    update on navbar

--- src/components/Navigation/TopMenu.tsx
+++ src/components/Navigation/TopMenu.tsx
@@ -12,28 +12,32 @@ interface ITopMenuProps {
 const TopMenu = (props: ITopMenuProps) => {
   const { name, navIsOpen, toggleNav, iconMenu, iconX } = props;
   return (
-    <div className="fixed top-0 left-0 z-50 flex flex-row items-center justify-between w-full h-20 px-2 bg-black text-orange">
-      <div className="z-20 text-base sm:text-2xl">
-        <span>{name}</span>
-      </div>
-      <div className="z-20 flex justify-end">
-        {navIsOpen ? (
-          <div onClick={() => toggleNav(false)}>
-            <img
-              alt="close"
-              className="w-6 h-6 cursor-pointer sm:w-8 sm:h-8"
-              src={iconX}
-            />
+    <div className="fixed top-0 left-0 z-50 flex flex-row items-center w-full h-20 px-2 bg-black text-orange">
+      <div className="container px-5 mx-auto">
+        <div className="flex flex-row items-center justify-between">
+          <div className="z-20 text-base sm:text-2xl">
+            <span>{name}</span>
           </div>
-        ) : (
-          <div onClick={() => toggleNav(true)}>
-            <img
-              alt="menu"
-              className="w-6 h-6 cursor-pointer sm:w-8 sm:h-8"
-              src={iconMenu}
-            />
+          <div className="z-20 flex justify-end">
+            {navIsOpen ? (
+              <div onClick={() => toggleNav(false)}>
+                <img
+                  alt="close"
+                  className="w-6 h-6 cursor-pointer sm:w-8 sm:h-8"
+                  src={iconX}
+                />
+              </div>
+            ) : (
+              <div onClick={() => toggleNav(true)}>
+                <img
+                  alt="menu"
+                  className="w-6 h-6 cursor-pointer sm:w-8 sm:h-8"
+                  src={iconMenu}
+                />
+              </div>
+            )}
           </div>
-        )}
+        </div>
       </div>
     </div>
   );
