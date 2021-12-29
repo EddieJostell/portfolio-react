b68a05466d7dc40ad8d0f3bb0894fee0eed390ab
Eddie <eddie.jostell@gmail.com>
2021-12-29 03:50:10 +0100

    Background gradient

--- src/utils/Route.ts
+++ src/utils/Route.ts
@@ -1,6 +1,6 @@
 import { FunctionComponent } from 'react';
-import About from '../components/About/About';
 import Contact from '../components/Contact/Contact';
+import Home from '../components/Home/Home';
 import Portfolio from '../components/Portfolio/Portfolio';
 
 export interface IRouteInfoItem {
@@ -15,7 +15,7 @@ export const RouteInfo: IRouteInfoItem[] = [
     id: 1,
     path: '/',
     exact: true,
-    component: About,
+    component: Home,
   },
   {
     id: 2,
