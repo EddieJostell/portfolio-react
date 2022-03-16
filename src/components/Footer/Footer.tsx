445fcf81cd19c457a410307eb436ee9f70d0917e
Eddie <edward.kelder.jostell@skandia.se>
2022-03-16 16:19:56 +0100

    Footer  scrollTop logic

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,45 @@
+import { motion } from 'framer-motion';
 import React from 'react';
 import { ContactSlim } from '../Contact/ContactSlim';
 import { Container } from '../Container/Container';
 import './Footer.scss';
 
 export const Footer = () => {
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  const topMotion = {
+    rest: { x: -15, opacity: 0, ease: 'easeOut', duration: 0.2, type: 'tween' },
+    hover: {
+      x: 8,
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        type: 'tween',
+        ease: 'easeIn',
+      },
+    },
+  };
+
   return (
     <div className='Footer'>
       <Container>
-        <div className='Footer-name'>E</div>
-        <ContactSlim icons={false} />
+        <div className='Footer-wrapper'>
+          <motion.div
+            initial='rest'
+            animate='rest'
+            className='Footer-wrapper-name'
+            whileHover='hover'
+            onClick={scrollTop}
+          >
+            E
+            <motion.div className='scroll-to-top' variants={topMotion}>
+              Top
+            </motion.div>
+          </motion.div>
+          <ContactSlim icons={false} />
+        </div>
       </Container>
     </div>
   );
