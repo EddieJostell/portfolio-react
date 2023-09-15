71f3aaaa1120e442a5040374aaf9b63e7e7bae28
Edward Kelder Jostell <Edward.Kelder.Jostell@skandia.se>
2023-09-15 16:09:49 +0200

    emailjs code goes live

--- src/components/Contact/ContactForm/ContactForm.tsx
+++ src/components/Contact/ContactForm/ContactForm.tsx
@@ -67,7 +67,7 @@ export const ContactForm = (props: IContactFormProps) => {
     // REAL EMAIL JS CODE
     if (data) {
       dispatch({ type: "SUBMIT" });
-      /*   emailjs.sendForm(serviceID(), templateID(), form.current!, userID()).then(
+      emailjs.sendForm(serviceID(), templateID(), form.current!, userID()).then(
         (result) => {
           console.log("SUCCESS!", result.status, result.text);
           dispatch({ type: "SUCCESS" });
@@ -76,15 +76,15 @@ export const ContactForm = (props: IContactFormProps) => {
           console.log("FAILED...", error.text);
           dispatch({ type: "ERROR" });
         }
-      ); */
+      );
 
-      setTimeout(() => {
+      /*  setTimeout(() => {
         if (1 + 1 === 3) {
           dispatch({ type: "SUCCESS" });
         } else {
           dispatch({ type: "ERROR" });
         }
-      }, 2000);
+      }, 2000); */
     }
   };
 
