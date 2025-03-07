2705285d2fc4ba52e6c590a2b3b05fafff9f90a9
Edward Kelder Jostell <Edward.Kelder.Jostell@skandia.se>
2025-03-07 16:17:27 +0100

    Installed compression plugin for vite: vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(),
+        viteCompression({
+            algorithm: 'gzip', // Du kan också använda 'brotliCompress' eller 'deflate'
+          }),
+    ],
     server: {
         port: 8000,
         open: true,
