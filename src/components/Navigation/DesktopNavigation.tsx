5a9f9b7ccf076ebeee4870ef95db1645d3f994ff
Edward Kelder Jostell <Edward.Kelder.Jostell@skandia.se>
2025-06-17 15:56:29 +0200

    Remove unused imports

--- src/components/Navigation/DesktopNavigation.tsx
+++ src/components/Navigation/DesktopNavigation.tsx
@@ -1,21 +1,9 @@
-import React, { FC, useContext, useState } from 'react';
-import { HelperContext, IContextState } from '../../utils/HelperContext';
-import { INavLinkItem } from '../../utils/data';
-import { Link } from 'react-scroll';
-import { scrollTop, useMediaQuery } from '../../utils/hooks';
-import PDF from '../../documents/CV_Eddie_Jostell.pdf';
-import { motion } from 'framer-motion';
-import { NavLinksAnimation, NavNameAnimation } from './NavAnimations';
+import React, { FC } from 'react';
+
+import { NavLinksAnimation } from './NavAnimations';
 import {
-  ContactLink,
-  NavigationButton,
-  NavigationLink,
-  StyledIcon,
-  StyledIconContainer,
   StyledNavigationContainer,
   StyledNavMenu,
-  StyledNavMenuLink,
-  StyledTopNavigation,
 } from './StyledNavigationElements';
 
 interface IDesktopNavigationProps {
