23229e68f21de42d6c00d659ee83a58bfdfc9983
Eddie <eddie.jostell@gmail.com>
2025-06-20 16:45:37 +0200

    feat: add SocialMedia component and remove SocialMediaLink component

---
+++ src/components/Contact/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react';
+
+interface ISocialMediaProps {}
+
+export const SocialMedia: FC<ISocialMediaProps> = ({}) => {
+  return <div>SocialMedia</div>;
+};
--- src/components/Contact/SocialMediaLinks/SocialMediaLink.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FC } from 'react';
-
-interface ISocialMediaLinkProps {}
-
-export const SocialMediaLink: FC<ISocialMediaLinkProps> = ({}) => {
-  return <div>SocialMediaLinks</div>;
-};
