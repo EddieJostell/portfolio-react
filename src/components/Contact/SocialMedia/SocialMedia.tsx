63345f9ecb59f4e350123e79c605da962a8fa789
Edward Kelder Jostell <Edward.Kelder.Jostell@skandia.se>
2025-07-15 14:48:34 +0200

    WIP SocialMedia component

--- src/components/Contact/SocialMedia/SocialMedia.tsx
+++ src/components/Contact/SocialMedia/SocialMedia.tsx
@@ -1,4 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
+import { HelperContext, IContextState } from '../../../utils/HelperContext';
+import { ISocialMediaLink } from '../../../utils/data';
 
 interface ISocialMediaProps {
   icons?: boolean;
@@ -6,5 +8,36 @@ interface ISocialMediaProps {
 }
 
 export const SocialMedia: FC<ISocialMediaProps> = ({ icons, links }) => {
-  return <div>SocialMedia</div>;
+  const socialLinks = useContext<IContextState>(HelperContext);
+
+  const mapLinksOrIcons = socialLinks.socialItem.map(
+    (item: ISocialMediaLink) => {
+      return (
+        <>
+          {icons && (
+            <a
+              href={item.link}
+              target='_blank'
+              rel='noopener noreferrer'
+              key={item.id}
+            >
+              {item.iconSrc}
+            </a>
+          )}
+          {links && (
+            <a
+              href={item.link}
+              target='_blank'
+              rel='noopener noreferrer'
+              key={item.id}
+            >
+              <span>{item.title}</span>
+            </a>
+          )}
+        </>
+      );
+    }
+  );
+
+  return <div>{mapLinksOrIcons}</div>;
 };
