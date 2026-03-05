a336aca988760e42c083498a759b7c39f8ef4497
Edward Kelder Jostell <edward.kelder.jostell@skandia.se>
2026-03-05 11:17:29 +0100

    Added aria-labels to project links to fix axe-core accessibility check.

--- src/components/Portfolio/PortfolioContent/PortfolioContent.tsx
+++ src/components/Portfolio/PortfolioContent/PortfolioContent.tsx
@@ -29,18 +29,23 @@ export const PortfolioContent = (props: IPortfolioContent) => {
           </div>
           <div className='links'>
             {github && (
-              <a href={github} target='_blank' rel='noreferrer'>
+              <a
+                href={github}
+                target='_blank'
+                rel='noreferrer'
+                aria-label='GitHub repository'
+              >
                 <GitHub color='white' size={'40px'} />
               </a>
             )}
-            <a href={link} target='_blank' rel='noreferrer'>
+            <a href={link} target='_blank' rel='noreferrer' aria-label={title}>
               <ExternalLink color='white' size={'40px'} />
             </a>
           </div>
         </div>
         <div className='body'>
           <h1 className='title'>
-            <a href={link} target='_blank' rel='noreferrer'>
+            <a href={link} target='_blank' rel='noreferrer' aria-label={title}>
               {title}
             </a>
           </h1>
