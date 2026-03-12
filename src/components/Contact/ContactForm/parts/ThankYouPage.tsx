ec3e1a8668860ea29ddbf54a39446b3e7ec9bc22
Edward Kelder Jostell <edward.kelder.jostell@skandia.se>
2026-03-12 14:57:50 +0100

    refactor(ThankYouPage): Simplify structure by removing unnecessary div wrapper.

--- src/components/Contact/ContactForm/parts/ThankYouPage.tsx
+++ src/components/Contact/ContactForm/parts/ThankYouPage.tsx
@@ -14,24 +14,21 @@ export const ThankYouPage = (props: ThankYouPageProps) => {
   }, []);
 
   return (
-    <div className='Form'>
-      <div className='Form-thanks' role='alert' aria-live='polite'>
-        <div className='title'>Thanks</div>
-        <div className='description'>
-          <h1 ref={headingRef} tabIndex={-1}>
-            Message has been sent!
-          </h1>
-          Thank you for reaching out! I will get back at you as soon as
-          possible!
-        </div>
-        <button
-          className='btn'
-          onClick={toggleContact}
-          aria-label='Close contact form and return to start'
-        >
-          Back to start
-        </button>
+    <div className='Form-thanks' role='alert' aria-live='polite'>
+      <div className='title'>Thanks</div>
+      <div className='description'>
+        <h1 ref={headingRef} tabIndex={-1}>
+          Message has been sent!
+        </h1>
+        Thank you for reaching out! I will get back at you as soon as possible!
       </div>
+      <button
+        className='btn'
+        onClick={toggleContact}
+        aria-label='Close contact form and return to start'
+      >
+        Back to start
+      </button>
     </div>
   );
 };
